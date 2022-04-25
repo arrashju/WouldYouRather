e53eb2c53b761656b4a1f4515024f53c729324fe
arrashju <justin.ar.rasheed@gmail.com>
2022-04-25 17:08:47 -0400

    attributed person who designed avatars

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -6,11 +6,27 @@ const Footer = (props) => {
       <Container>
         <Foot className="footer">
           <P>Employee Poll</P>
+          <P>
+            Avatar pack vector created by pikisuperstar -{" "}
+            <A href="https://www.freepik.com/vectors/avatar-pack">
+              www.freepik.com
+            </A>
+          </P>
         </Foot>
       </Container>
     </>
   );
 };
+
+const A = styled.a`
+  font-weight: 500;
+
+  :hover {
+    text-decoration: underline;
+  }
+
+  transition: text-decoration 0.2s ease;
+`;
 
 const Container = styled.div`
   background: #fcfdff;
@@ -24,6 +40,15 @@ const Foot = styled.footer`
 const P = styled.p`
   padding: 0;
   margin: 0;
+
+  :not(last-child) {
+    margin-bottom: 15px;
+  }
+
+  :last-child {
+    color: #444;
+    font-size: 13px;
+  }
   text-align: center;
   font-size: 14px;
 `;
